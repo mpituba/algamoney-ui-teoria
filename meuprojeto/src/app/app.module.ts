98fb2ec46a633da1638e9eac15b2abfc70fd8793
Marcelo Pituba <mpituba@yahoo.com>
2022-07-02 13:59:33 -0300

    9.3.Criando um componente

--- meuprojeto/src/app/app.module.ts
+++ meuprojeto/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HelloComponent } from './hello/hello.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    BemVindoComponent
   ],
   imports: [
     BrowserModule
