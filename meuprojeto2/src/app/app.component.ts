761fcf3805c5c74032856f969f14a4cfed3568f3
Marcelo Pituba <mpituba@yahoo.com>
2022-07-04 14:34:14 -0300

    09.09. Usando property bindind - meuprojeto2

--- meuprojeto2/src/app/app.component.ts
+++ meuprojeto2/src/app/app.component.ts
@@ -9,9 +9,15 @@ export class AppComponent {
   title = 'meuprojeto2';
   nome = 'Marcelo';
 
-  adicionar(nome: string) {
-    this.nome = nome;
+  adicionar() {
+    console.log(`Adicionando ${this.nome}`);
+
+    const numero = Math.round(Math.random() * 100);
+    this.nome = 'João ' + numero;
   }
 
+  alterarNome(event: any) {
+    this.nome = event.target.value;
+  }
 
 }
